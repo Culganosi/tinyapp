4a02b58f89f5ae686540d4ec2d87ca7a6553b39d
Culganosi <culgan_@hotmail.com>
2022-03-21 14:16:42 -0400

    before adding POST route

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3030; // default port 8080
+const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -8,8 +8,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
